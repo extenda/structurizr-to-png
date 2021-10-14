69fc9dc27f9016fa2bda61725c3cc04df4a9aae4
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2021-10-14 15:35:24 +0200

    fix: Use readFile as readFileSync intermittently returned empty data

--- install-tools.sh
+++ install-tools.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+rm -rf "$SCRIPT_PATH/lib"
 mkdir -p "$SCRIPT_PATH/lib"
 
-plantUmlVersion="1.2021.9"
+plantUmlVersion="1.2021.12"
 curl -sS "https://repo.maven.apache.org/maven2/net/sourceforge/plantuml/plantuml/$plantUmlVersion/plantuml-${plantUmlVersion}.jar" -o "$SCRIPT_PATH/lib/plantuml.jar"
 
 structurizrVersion="1.15.0"
