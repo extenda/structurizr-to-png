54cee7cc67e34fec0a814bce6e16cb4bcabfcaf0
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2021-10-14 15:37:11 +0200

    fix: Use readFile as readFileSync intermittently returned empty data (#5)

--- src/plantuml.js
+++ src/plantuml.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const { spawn } = require('child_process');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 const axios = require('axios');
 const plantUmlEncoder = require('plantuml-encoder');
 const log = require('./logger');
@@ -37,14 +37,15 @@ const renderPng = async (pumlFile) => {
   // Wait for PlantUML to be ready for requests.
   await serverReady;
 
-  const data = plantUmlEncoder.encode(fs.readFileSync(pumlFile, 'utf8'));
-  return axios.get(
+  return fs.readFile(pumlFile, 'utf8')
+    .then(plantUmlEncoder.encode)
+    .then((data) => axios.get(
     `http://127.0.0.1:8888/plantuml/png/${data}`,
     { responseType: 'arraybuffer' },
     ).then((response) => ({
       imageName: path.basename(pumlFile, '.puml') + '.png',
       data: response.data
-    }));
+    })));
 }
 
 module.exports = {
